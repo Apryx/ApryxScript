0d9b85e7152d43787c10da472fd2fc1ca5a90243
JustF <408992@student.saxion.nl>
2016-05-04 09:32:08 +0200

    Updated the parser with some minor expression parsing (parseExpression())

--- src/language/Parser.java
+++ src/language/Parser.java
@@ -21,6 +21,7 @@ public class Parser {
 		Context context = new Context();
 		
 		//TODO parse the context
+		parseStatement(context);
 		
 		return context;
 	}
@@ -32,7 +33,12 @@ public class Parser {
 		}
 		else if(current.getType() == TokenType.IDENTIFIER){
 			context.addStatement(new ExpressionStatement(parseExpression()));
-		}else{
+		}
+		else if(current.getType() == TokenType.LINE_END){
+			//Consume the line-end
+			lexer.next();
+		}
+		else{
 			throw new UnexpectedTokenException(current, TokenType.IDENTIFIER, TokenType.KEYWORD);
 		}
 	}
@@ -42,6 +48,30 @@ public class Parser {
 	}
 	
 	public Expression parseExpression(){
+		Token start = lexer.current();
+		
+		//TODO parse lhs and rhs
+		//Have a function that only parses the identifier, int, string, float, etc.
+		
+		if(start.getType() == TokenType.IDENTIFIER){
+			Token operator = lexer.next();
+			
+			//Function call
+			if(operator.getType() == TokenType.BRACKET_OPEN){
+				
+			}
+			
+			//Set operator
+			else if(operator.getType() == TokenType.EQUALS){
+				
+			}
+			
+			else{
+				throw new UnexpectedTokenException(operator, TokenType.BRACKET_OPEN, TokenType.IDENTIFIER);
+			}
+		}
+		
+		
 		return null;
 	}
 	
