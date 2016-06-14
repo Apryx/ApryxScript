a71f4d9a6010b3d1f39a2c8cf7a599eef69ccd91
JustF <folkerthoogenraad@hotmail.nl>
2016-06-14 14:38:37 +0200

    Added Type class

---
+++ src/context/Type.java
@@ -0,0 +1,29 @@
+package context;
+
+public class Type {
+	
+	private String name;
+
+	public Type(String name){
+		this.name = name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Type other = (Type) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	
+	
+}
